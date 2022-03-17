d4eead658ed454ad84c64a40e0c49bcbbfc055c6
SouhaibESS <souhaibue@gmail.com>
2022-03-17 10:11:34 +0100

    add create promotion page instead of pop-up

--- src/screens/Promotions/Create/index.js
+++ src/screens/Promotions/Create/index.js
@@ -1,35 +1,38 @@
 import React, { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
+import _get from "lodash/get";
+import { useHistory } from "react-router-dom";
+
 import { get as formations } from "../../../store/actions/formation";
 import { get as promotions } from "../../../store/actions/promotion";
 import { get as teachers } from "../../../store/actions/teacher";
 import { getSalles as salles } from "../../../store/actions/promotion";
 import { create } from "../../../store/actions/promotion";
-import _get from "lodash/get";
 
 import View from "./view";
 
-import { DEFAULT_MESSAGES, TYPES } from "../../../utils/constants";
+import { DEFAULT_MESSAGES, PATHS, TYPES } from "../../../utils/constants";
 import { openNotification } from "../../../utils/helpers";
 
-const Create = ({ handleClose }) => {
+const Create = () => {
   const dispatch = useDispatch();
+  const { push } = useHistory();
 
   const formationsQuery = useSelector((state) => state.formation.get);
   const teacherQuery = useSelector((state) => state.teacher.get);
   const createQuery = useSelector((state) => state.promotion.create);
   const sallesQuery = useSelector((state) => state.promotion.getSalles);
 
-  const onCreate = (data, onSuccessCallBack, onErrorCallBack) =>
+  const onCreate = (data) =>
     dispatch(
       create(
         data,
         () => {
-          handleClose();
           openNotification({
             type: TYPES.SUCCESS,
             message: DEFAULT_MESSAGES.SUCCESS,
           });
+          push(PATHS.HOME);
           dispatch(promotions());
         },
         (errors) =>
@@ -39,6 +42,8 @@ const Create = ({ handleClose }) => {
           })
       )
     );
+
+  const onRetourClick = () => push(PATHS.HOME);
 
   useEffect(() => {
     dispatch(formations());
@@ -60,7 +65,7 @@ const Create = ({ handleClose }) => {
         onCreate,
         createQuery,
         sallesQuery,
-        handleClose,
+        onRetourClick,
       }}
     />
   );
--- src/screens/Promotions/List/index.js
+++ src/screens/Promotions/List/index.js
@@ -1,14 +1,16 @@
 import React, { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
+import { useHistory } from "react-router-dom";
 
 import { get, changeProcess } from "../../../store/actions/promotion";
 import { openNotification } from "../../../utils/helpers";
-import { TYPES, DEFAULT_MESSAGES } from "../../../utils/constants";
+import { TYPES, DEFAULT_MESSAGES, PATHS } from "../../../utils/constants";
 
 import View from "./view";
 
 const List = () => {
   const dispatch = useDispatch();
+  const { push } = useHistory();
 
   const promotionsQuery = useSelector((state) => state.promotion.get);
   const processQuery = useSelector((state) => state.promotion.changeProcess);
@@ -34,11 +36,20 @@ const List = () => {
     );
   };
 
+  const onClickCreate = () => {
+    console.log("create clicked!");
+    push(PATHS.PROMOTIONS.CREATE);
+  };
+
   useEffect(() => {
     dispatch(get());
   }, [dispatch]);
 
-  return <View {...{ promotionsQuery, onChangeProcess, processQuery }} />;
+  return (
+    <View
+      {...{ promotionsQuery, onChangeProcess, processQuery, onClickCreate }}
+    />
+  );
 };
 
 export default List;
