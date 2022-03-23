2830508c3d6b4f60038faadfffa0468dbbcaebe6
SouhaibESS <souhaibue@gmail.com>
2022-03-23 09:47:10 +0100

    change detail view from export function to export const

--- src/screens/UEs/Detail/view.js
+++ src/screens/UEs/Detail/view.js
@@ -1,7 +1,7 @@
-import React from 'react'
+import React from "react";
 
-export default function view() {
-  return (
-	<div>view</div>
-  )
-}
+const View = () => {
+  return <div>view</div>;
+};
+
+export default View;
