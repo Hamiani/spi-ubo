4a649d5b052c11279c7355818115657d5a90b5e1
SouhaibESS <souhaibue@gmail.com>
2022-03-23 14:27:55 +0100

    add ue details page design

--- src/screens/Routes/index.js
+++ src/screens/Routes/index.js
@@ -3,10 +3,12 @@
 import NotFound from "../../Shared/NotFound";
 import Teachers from "../Teachers";
 import Promotions from "../Promotions";
+import UEs from "../UEs"
 
 const routes = [
   ...Promotions,
   ...Teachers,
+  ...UEs,
   { component: NotFound },
 ];
 
