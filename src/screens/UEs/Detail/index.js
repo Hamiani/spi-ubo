978fbdb819fa6be48407e0bbda0931f6be4ca36a
SouhaibESS <souhaibue@gmail.com>
2022-03-23 16:52:54 +0100

    add 'unite d'enseignement details' page

--- src/screens/UEs/Detail/index.js
+++ src/screens/UEs/Detail/index.js
@@ -1,8 +1,24 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { useParams, useHistory } from "react-router-dom";
+import { getOne } from "../../../store/actions/ue";
+
 import View from "./View";
 
 const Detail = () => {
-  return <View />
+  const { id } = useParams();
+  const { goBack } = useHistory();
+  const dispatch = useDispatch();
+
+  const ueQuery = useSelector((state) => state.ue.getOne);
+
+  const onGoBack = () => goBack();
+
+  useEffect(() => {
+    dispatch(getOne());
+  }, [dispatch, id]);
+
+  return <View {...{ueQuery, onGoBack}} />;
 };
 
 export default Detail;
