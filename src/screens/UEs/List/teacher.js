edac248cad337f5b417396eea962f438c7b1af06
AKKAR Ferdaous <46265098+ferdaousak@users.noreply.github.com>
2022-03-31 11:21:15 +0200

    changed to my old table

--- src/screens/UEs/List/teacher.js
+++ src/screens/UEs/List/teacher.js
@@ -1,71 +1,97 @@
 import React from "react";
-import { Empty, Table, Tooltip } from "antd";
+import { Dropdown, Empty, Menu, Table, Tooltip } from "antd";
+import { BsThreeDots } from "react-icons/bs";
+import { EyeOutlined} from "@ant-design/icons";
+import className from "classnames";
 
 import get from "lodash/get";
+import { isEvenNumber } from "../../../utils/helpers";
 
-const columns = () => [
+
+const renderHours = (n) => <>{n} h</>
+const menu = ({ record, onShow }) => (
+  <Menu>
+    <Menu.Item key="0" onClick={() => onShow(get(record, "id",{}))} >
+      <EyeOutlined />
+      Afficher
+    </Menu.Item>
+  </Menu>
+);
+
+const columns = ({onShow}) => [
   {
     title: "Formation",
-    dataIndex: "code_Formation",
+    dataIndex: "id",
     key: "formation",
-    sorter: (a, b) =>
-      get(a, "code_Formation", "").localeCompare(get(b, "code_Formation", "")),
+    sorter: (a, b) => get(a, "id.code_Formation", "").localeCompare(get(b, "id.code_Formation", "")),
+    render: (record) => record.code_Formation,
     defaultSortOrder: "ascend",
-    render: (_, record) => get(record, "id.code_Formation", ""),
   },
   {
     title: <Tooltip title="Unités d'enseignement">UE</Tooltip>,
-    dataIndex: "designation",
-    key: "designation",
-    sorter: (a, b) =>
-      get(a, "designation", "").localeCompare(get(b, "designation", "")),
+    dataIndex: "id",
+    key: "ue",
+    sorter: (a, b) => get(a, "id.code_ue", "").localeCompare(get(b, "id.code_ue", "")),
+    render: (id, row) =>{
+      return <Tooltip title={row.designation}>{id.code_ue}</Tooltip>; 
+    } ,
     defaultSortOrder: "ascend",
   },
   {
-    title: (
-      <Tooltip title="Nombre d'heure cours magistraux">
-        Nombre d'heure CM
-      </Tooltip>
-    ),
-    dataIndex: "nbh_Cm",
+    title: <Tooltip title="Nombre d'heures des cours magistraux">CM</Tooltip>,
+    dataIndex: "nbh_cm",
     key: "cm",
+    render : (text) => renderHours(text)
   },
   {
-    title: (
-      <Tooltip title="Nombre d'heure travaux dirigés">
-        Nombre d'heure TD
-      </Tooltip>
-    ),
-    dataIndex: "nbh_Td",
+    title: <Tooltip title="Nombre d'heures des travaux dirigés">TD</Tooltip>,
+    dataIndex: "nbh_td",
     key: "td",
+    render : (text) => renderHours(text)
   },
   {
-    title: (
-      <Tooltip title="Nombre d'heure travaux pratiques">
-        Nombre d'heure TP
-      </Tooltip>
-    ),
-    dataIndex: "nbh_Tp",
+    title: <Tooltip title="Nombre d'heures des travaux pratiques">TP</Tooltip>,
+    dataIndex: "nbh_tp",
     key: "tp",
+    render : (text) => renderHours(text)
   },
   {
-    title: (
-      <Tooltip title="Nombre d'heure équivalent travaux dirigés">
-        Nombre d'heure ETD
-      </Tooltip>
-    ),
-    dataIndex: "nbh_Etd",
+    title: <Tooltip title="Nombre d'heures ETD">ETD</Tooltip>,
+    dataIndex: "nbh_etd",
     key: "etd",
+    render : (text) => renderHours(text)
+  },
+  {
+    title: "Actions",
+    dataIndex: "actions",
+    key: "actions",
+    align: "center",
+    render: (_, record) => (
+      <Dropdown overlay={menu({onShow, record})} trigger={["click"]}>
+        <BsThreeDots className="fa-icon" size={23} />
+      </Dropdown>
+    ),
   },
 ];
 
-const View = ({ data }) => {
+const View = ({ data, onShow }) => {
+  console.log("data", data);
+
   return (
     <Table
-      rowKey={(record) =>
-        `${get(record, "id.code_Formation") + get(record, "id.code_Ue")}`
+    rowKey={(record) =>
+      `${
+        get(record, "id.code_Formation") +
+        get(record, "id.code_ue")
+      }`
+    }
+      columns={columns({ onShow })}
+      rowClassName={(_, index) =>
+        className({
+          "table-row-dark": isEvenNumber(index),
+          "table-row-light": !isEvenNumber(index),
+        })
       }
-      columns={columns()}
       dataSource={data}
       showSorterTooltip={false}
       pagination={false}
