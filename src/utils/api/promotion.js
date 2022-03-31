285a0c8580cfd578847da0e23103d0c1cf2e8e27
hamiani akbi <hamiani.akbi@gmail.com>
2022-03-31 14:18:02 +0200

    fix api url

--- src/utils/api/promotion.js
+++ src/utils/api/promotion.js
@@ -6,12 +6,15 @@ const { get, getOne, changeProcess, getSalles } = mock;
 const candidatApi = {
   //get,
   get: () => api.get("/promotions"),
+  
   //getSalles,
   getSalles: () => api.get("/salles"),
+  
   //getOne,
-  getOne: ({ codeFormation, anneeUniversitaire }) =>
-    api.get(`/promotions/${codeFormation}/${anneeUniversitaire}`),
+  getOne: ({ code_Formation, annee_Universitaire }) =>api.get(`/promotions/${code_Formation}/${annee_Universitaire}`),
+  
   create: (data) => api.post("/promotions", { data }),
+  
   remove: ({ code_Formation, annee_Universitaire }) =>
     api.del(`/promotions/${code_Formation}/${annee_Universitaire}`),
   //changeProcess,
