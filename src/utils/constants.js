4738dd7172c183de74ac2d309214800a5c3a9ad0
hamiani akbi <hamiani.akbi@gmail.com>
2022-04-01 02:24:31 +0200

    update page url

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -12,26 +12,26 @@ export const PATHS = {
     LIST: "/enseignants",
     CREATE: "/enseignants/create",
     DETAIL: "/enseignants/:id",
-    UPDATE: "/enseignants/update/:id",
+    UPDATE: "/enseignants/update/:id"
   },
   PROMOTIONS: {
     LIST: "/promotions",
     CREATE: "/promotions/create",
-    DETAIL: "/promotions/:code_Formation/:annee_Universitaire",
+    DETAIL: "/promotions/:code_Formation/:annee_Universitaire"
   },
 
   UES: {
     LIST: "/ues",
-    DETAIL: "/ues/:code_Formation/:code_Ue",
+    DETAIL: "/ues/:code_Formation/:code_Ue"
   },
 
   ETUDIANTS: {
     LIST: "/etudiants",
     DETAIL: "/etudiants/:id",
-    UPDATE: "/etudiants/update/:id",
+    UPDATE: "/etudiants/update/:code_Formation/:annee_Universitaire/:id"
   },
 
-  HOME: "/",
+  HOME: "/"
   /*   REGISTRATION: "/register",
   LOGIN: "/login",
   LOGOUT: "/logout", */
@@ -39,22 +39,23 @@ export const PATHS = {
 
 export const RESPONSE_TYPE = {
   JSON: "json",
-  NONE: "none",
+  NONE: "none"
 };
 
 export const ROUTES_ACCESS = {
   PUBLIC: "public",
-  PRIVATE: "private",
+  PRIVATE: "private"
 };
 
 export const DATE_FORMAT = "DD/MM/YYYY";
+
 export const DATE_MESSAGES = {
   dateReponseLalp:
     "La date de réponse liste d'attente doit être supérieur à la date réponse liste principale et inférieur à la date de rentrée",
   dateReponseLp:
     "La date de réponse liste principale doit être inférieur à la date réponse liste d'attente et inférieur à la date de rentrée",
   dateRentree:
-    "La date de rentrée doit être supérieur à la date liste principale et la date réponse liste d'attente",
+    "La date de rentrée doit être supérieur à la date liste principale et la date réponse liste d'attente"
 };
 
 export const ALL = "all";
@@ -65,52 +66,52 @@ export const PROCESSUS_STAGE = {
   RECH: {
     KEY: "RECH",
     VALUE: "Recherche en cours",
-    NEXT: "EC",
+    NEXT: "EC"
   },
   EC: {
     KEY: "EC",
     VALUE: "Stage en cours",
-    NEXT: "TUT",
+    NEXT: "TUT"
   },
   TUT: {
     KEY: "TUT",
     VALUE: "Tuteurs attribués",
-    NEXT: "SOUT",
+    NEXT: "SOUT"
   },
   SOUT: {
     KEY: "SOUT",
     VALUE: "Sessions de soutenance définies",
-    NEXT: "EVAL",
+    NEXT: "EVAL"
   },
   EVAL: {
     KEY: "EVAL",
     VALUE: "Stage évalué",
-    NEXT: "EVAL",
-  },
+    NEXT: "EVAL"
+  }
 };
 export const PROCESSUS = {
   RECH: { next: "EC" },
   EC: { next: "TUT" },
   TUT: { next: "SOUT" },
   SOUT: { next: "EVAL" },
-  EVAL: { next: "EVAL" },
+  EVAL: { next: "EVAL" }
 };
 
 export const TYPES = {
   SUCCESS: "success",
-  ERROR: "error",
+  ERROR: "error"
 };
 
 export const DEFAULT_MESSAGES = {
   SUCCESS: "Opération effectuée avec succès",
-  ERROR: "Un problème est survenu : ",
+  ERROR: "Un problème est survenu : "
 };
 
 export const PROMOTION_TEXTS = {
   PROCESSUS_STAGE: {
     TOOLTIP: "Avancer le processus de stage",
-    CHANGE_PROCESS: "Êtes-vous sûr de vouloir avancer le processus de stage ?",
-  },
+    CHANGE_PROCESS: "Êtes-vous sûr de vouloir avancer le processus de stage ?"
+  }
 };
 
 export const UBO_LINKS = {
@@ -118,21 +119,21 @@ export const UBO_LINKS = {
   LINKEDIN: "https://www.linkedin.com/school/ubo/?originalSubdomain=fr",
   MAIL: "mailto:directeur.sciences@univ-brest.fr",
   YOUTUBE: "https://www.youtube.com/user/UnivBrest",
-  TWITER: "https://twitter.com/UBO_UnivBrest",
+  TWITER: "https://twitter.com/UBO_UnivBrest"
 };
 
 export const SEXES = {
   H: {
     key: "H",
-    value: "Homme",
+    value: "Homme"
   },
   F: {
     key: "F",
-    value: "Femme",
-  },
+    value: "Femme"
+  }
 };
 
 export const DETAIL_TYPES = {
   PROMOTION: "PROMOTION",
-  TEACHER: "TEACHER",
+  TEACHER: "TEACHER"
 };
